bb1131df385f9d5997b4dd6a3e2cfbfe1a093fa0
Sana Parveen <misana348@gmail.com>
2023-05-28 19:55:26 +0530

    first commit

---
+++ frontend/src/main.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import store from "../store.js";
+import { Provider } from "react-redux";
+import App from "./App.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import PrivateRoute from "./components/PrivateRoute.jsx";
+import Homescreen from "./screens/Homescreen.jsx";
+import LoginScreen from "./screens/LoginScreen.jsx";
+import RegisterScreen from "./screens/RegisterScreen.jsx";
+import ProfileScreen from "./screens/ProfileScreen.jsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<Homescreen />} />
+      <Route path="/login" element={<LoginScreen />} />
+      <Route  path="/register" element={<RegisterScreen />} />
+      {/*Private Routes*/}
+      <Route  path="" element={<PrivateRoute />} />
+      <Route  path="/profile" element={<ProfileScreen />} />
+    </Route>
+    
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store ={store}>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+  </Provider>
+);
---
+++ frontend/src/screens/Homescreen.jsx
@@ -0,0 +1,8 @@
+import Hero from "../components/Hero"
+const Homescreen = () => {
+  return (
+    <Hero/>
+  )
+}
+
+export default Homescreen; 
